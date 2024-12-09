7a076dc3c3687e9b1b9b74e3b4dab4132d6d491b
Arnav Patil <83710256+ZeroDeaths379@users.noreply.github.com>
2024-12-09 23:34:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -186,3 +186,16 @@ document.addEventListener("DOMContentLoaded", () => {
   // Load saved data
   loadWorkoutProgress();
 });
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('./service-worker.js')
+      .then((registration) => {
+        console.log('Service Worker registered: ', registration);
+      })
+      .catch((error) => {
+        console.log('Service Worker registration failed: ', error);
+      });
+  });
+}
